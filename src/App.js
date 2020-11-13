784418f1c79eac2f12de4fc55dd9a0fe29f7affb
INDRA\dsenam <dsenam@indracompany.com>
2020-11-13 16:17:21 -0300

    limpeza de estrutura, requisição axios om

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+
+// import { Container } from './styles';
+
+function Coin({image, name, symbol, price, volume}) {
+  return (
+    <>
+      <div className="coin-container">
+        <div className="coin-row">
+          <div className="coin">
+            <img src={image} alt="crypto"/>
+            <h1>{name}</h1>
+            <p className='coin-symbol'>{symbol}</p>
+          </div>
+          <div className="coin-data">
+            <p className="coin-prince">R${price}</p>
+            <p className="coin-volume">R${volume.toLocaleString()}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 function App() {
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+      setCoins(res.data)
+    }).catch(e => toast.error('Erro ao realizar requisição'))
+  }, [])
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => 
+      coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <ToastContainer />
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1>Search a currency</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input" onChange={handleChange}/>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} volume={coin.market_cap}/>
+            
+        )
+      })}
     </div>
+      
+    </>
   );
 }
 
